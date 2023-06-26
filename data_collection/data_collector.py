a7662956758d9fbf6449ee9dedfc47b45b9c8753
bradunaway3 <bdunaway@manh.com>
2023-06-26 13:33:19 -0400

    dataset created in data.zip

--- data_collection/data_collector.py
+++ data_collection/data_collector.py
@@ -4,55 +4,94 @@ from concurrent.futures import ThreadPoolExecutor
 import logging
 import time
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import requests
 import json
 
-def parse_nasdaq(file_path: str) -> pd.DataFrame:
-    df: pd.DataFrame = pd.read_csv(file_path)
-    df.drop(labels=["Last Sale", "Net Change", "% Change", "Market Cap", "Country"], inplace=True, axis=1)
-    return df
 
-def get_historical_data(symbol: str, headers: dict, start_year: str, end_year) -> None:
-    try:
-        logging.info(f"Attempting to gather data for {symbol}")
-        r = requests.get(url=f"https://api.marketdata.app/v1/stocks/candles/D/{symbol}?from={start_year}-01-01&to={end_year}-01-01&limit=50000", headers=headers)
-        logging.info(f"Request status: {r.status_code}")
-        with open(f"../data/{symbol}_candle_history_{start_year}_to_{end_year}.json", "w") as output:
-            json.dump(r.json(), output)
-    except Exception as e:
-        logging.error(f"Could not receive history for {symbol}")
-        print(e)
-
-
-
-if __name__ == "__main__":
+def setup_config() -> None:
+    # Allow global access
+    global config, headers
+    
     # Load in configuration yaml file
     with open("./collection-config.yaml", "r") as stream:
-        config: dict = safe_load(stream)
+        config = safe_load(stream)
         
     # Set the logging configuration
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.WARNING,
         format=config["log-format"],
         datefmt=config["log-date-format"]
     )
     
     #Load request credentials
     with open("./credentials.json") as stream:
-        headers: dict = json.load(stream)
-    endpoint = "https://api.marketdata.app/v1/stocks/candles/D/{symbol}"
+        headers = json.load(stream)
     
-    #Extract symbols from text file
-    stock_data = parse_nasdaq("./nasdaq_stock_export.csv")
+
+def parse_nasdaq(file_path: str) -> pd.DataFrame:
+    df: pd.DataFrame = pd.read_csv(file_path)
+    df.drop(labels=["Last Sale", "Net Change", "% Change", "Market Cap", "Country"], inplace=True, axis=1)
+    df.set_index("Symbol", inplace=True)
+    df["IPO Year"] = df["IPO Year"].fillna(2001)
+    df["IPO Year"] = pd.to_datetime(df["IPO Year"], format="%Y")
+    return df
+
+
+def create_parameters(data: pd.DataFrame, symbol: str) -> tuple[list[str], list[str], list[str]]:
+    ipo_date: datetime = data.loc[symbol]["IPO Year"]
+    start_year: int = 2001 if int(ipo_date.year) <= 2001 else int(ipo_date.year)
+    symbols = []
+    start_dates = []
+    end_dates = []
+    for i in range(start_year, 2023):
+        symbols.append(symbol)
+        start_dates.append(f"{i}-01-01")
+        end_dates.append(f"{i+1}-01-01")
     
-    # get_historical_data("AAPL", headers)
+    return (symbols, start_dates, end_dates)
+
+
+
+def get_historical_data(symbol: str, headers: dict, start_date: str, end_date: str) -> None:
+    try:
+        logging.info(f"Attempting to gather data for {symbol}")
+        r = requests.get(url=f"https://api.marketdata.app/v1/stocks/candles/D/{symbol}?from={start_date}&to={end_date}&limit=50000", headers=headers)
+        json_contents = r.json()
+        if json_contents == {"s": "no_data", "prevTime": None, "nextTime": None}:
+            logging.warning(f"No data to retrieve for {symbol} in the year {start_date[0:4]}")
+        else:
+            with open(f"../data/{symbol}_history_{start_date[0:4]}.json", "w") as output:
+                json.dump(r.json(), output)
+    except Exception as e:
+        logging.error(f"Could not receive history for {symbol}")
+        logging.error(e)
+
+
+if __name__ == "__main__":
+    #Script configuration
+    setup_config()
     
-    # with ThreadPoolExecutor(100) as tpe:
-    #     _ = tpe.map(get_historical_data, stock_data["Symbol"].to_list(), [headers for _ in range(7731)], ["2010" for _ in range(7731)], ["2020" for _ in range(7731)])
-    #     print([f.result() for f in _])
+    #Extract symbols and IPO from csv
+    stock_data: pd.DataFrame = parse_nasdaq("./nasdaq_stock_export.csv")
     
-    with ThreadPoolExecutor(100) as tpe:
-        _ = tpe.map(get_historical_data, stock_data["Symbol"].to_list(), [headers for _ in range(7731)], ["2005" for _ in range(7731)], ["2010" for _ in range(7731)])
-        print([f.result() for f in _])
+    timestamp = datetime.now()
+    request_count = 0
+
+    # Loop through all symbols, using thread pool for mass requesting
+    for s in stock_data.index.to_list()[5000:]:
+        repeated_symbol, start_dates, end_dates = create_parameters(data=stock_data, symbol=s)
+        
+        if (request_count >= 50_000 and (timestamp + relativedelta(days=1)) > datetime.now()):
+            logging.warning(f"Request count is currently {request_count}, the start time was {timestamp.strftime('%Y-%m-%d %H:%M:%S')}.  Sleeping for 1 hour...")
+            while ((timestamp + relativedelta(days=1)) > datetime.now()):
+                time.sleep(5)
+            timestamp = datetime.now()
+            request_count = 0
+        
+        with ThreadPoolExecutor(100) as tpe:
+            _ = tpe.map(get_historical_data, repeated_symbol, [headers for _ in range(len(repeated_symbol))], start_dates, end_dates)
+            print([f for f in _])
+        request_count += len(repeated_symbol)
     
-    print("Done.")
+    print(f"Done. Sent {request_count} over {(datetime.now() - timestamp).seconds / 3600} hours.")
---
+++ data_collection/data_consolidation.py
@@ -0,0 +1,143 @@
+import os
+import pandas as pd
+from datetime import datetime
+import logging
+from yaml import safe_load
+import json
+from concurrent.futures import ThreadPoolExecutor
+
+
+def setup_config() -> None:
+    # Allow global access
+    global config, headers
+    
+    # Load in configuration yaml file
+    with open("./collection-config.yaml", "r") as stream:
+        config = safe_load(stream)
+        
+    # Set the logging configuration
+    logging.basicConfig(
+        level=logging.WARNING,
+        format=config["log-format"],
+        datefmt=config["log-date-format"]
+    )
+    
+    #Load request credentials
+    with open("./credentials.json") as stream:
+        headers = json.load(stream)
+
+
+def make_ticker_directories(dir_contents: list[str]) -> None:
+    ticker_set = set()
+    
+    for f in dir_contents:
+        
+        if f.split("_")[0] not in ticker_set:
+            ticker_set.add(f.split("_")[0])
+            os.mkdir(f"../data/{f.split('_')[0]}")
+        os.rename("../data/" + f, f"../data/{f.split('_')[0]}/{f.split('_')[-1][0:4]}")
+    pass
+
+def remove_small_data(minimum_files: int) -> None:
+    for dir in os.listdir("../data/"):
+        
+        if len(os.listdir(f"../data/{dir}")) < minimum_files:
+            
+            for f in os.listdir(f"../data/{dir}/"):
+                logging.warning(f"Removing '../data/{dir}/{f}'")
+                os.remove(f"../data/{dir}/{f}")
+                
+            os.rmdir(f"../data/{dir}")
+            logging.warning(f"Successfully removed '../data/{dir}'")
+
+def remove_incomplete_data() -> None:
+    
+    for dir in os.listdir("../data/"):
+        
+        if (not check_consecutive_years(f"../data/{dir}")):
+            
+            for f in os.listdir(f"../data/{dir}/"):
+                logging.warning(f"Removing '../data/{dir}/{f}'")
+                os.remove(f"../data/{dir}/{f}")
+                
+            os.rmdir(f"../data/{dir}")
+            logging.warning(f"Successfully removed '../data/{dir}'")
+
+
+
+def create_year_span_mapping(dir_path: str):
+    year_span: dict[str, list] = {}
+    
+    for dir in os.listdir(dir_path):
+        key: str = f"{sorted(os.listdir(f'../data/{dir}'))[0][0:4]}-{sorted(os.listdir(f'../data/{dir}'))[-1][0:4]}"
+        if (key not in year_span.keys()):
+            year_span[key] = [dir]
+        else:
+            year_span[key].append(dir)
+    
+    return year_span
+        
+        
+
+def check_consecutive_years(dir_path: str) -> bool:
+    files = sorted(os.listdir(dir_path))
+    consecutive_flag: bool = True
+    year: int = int(files[0][0:4])
+    
+    for i in range(len(files)):
+        consecutive_flag = (int(files[i][0:4]) == year)
+        if (not consecutive_flag):
+            return False
+        year += 1
+    
+    return True
+
+
+
+def combine_jsons_to_csv(dir_path: str, start: str, end: str) -> None:
+    
+    for dir in os.listdir(dir_path):
+        df = pd.DataFrame()
+        if (start+".json" in os.listdir(dir_path + f"/{dir}")) and (end+".json" in os.listdir(dir_path + f"/{dir}")):
+            try:
+                print(dir)
+                for file in os.listdir(dir_path + f"/{dir}"):
+                    df = pd.concat([df, pd.read_json(f"{dir_path}/{dir}/{file}")], axis=0)
+                df.drop("s", axis=1, inplace=True)
+                df.rename(columns={"o" : "Day Open Price", "c" : "Day Close Price", "h" : "Daily High Price", "l" : "Daily Low Price", "v" : "Volume", "t" : "Date"}, inplace=True)
+                df["Date"] = pd.to_datetime(df["Date"], unit="s")
+                df["Symbol"] = dir
+                df.sort_values(by=["Date"], inplace=True)
+                df.reset_index(inplace=True)
+                df.to_csv(f"{dir_path}/{dir}.csv")
+            except:
+                print(f"Issue consolidating {dir}")
+        
+
+def remove_non_csvs(path: str):
+    for element in os.listdir(path):
+        if not element[-4:] == ".csv":
+            for file in os.listdir(f"{path}/{element}"):
+                os.remove(f"{path}/{element}/{file}")
+            os.rmdir(f"{path}/{element}")
+            
+
+def join_csvs(path: str):
+    df = pd.DataFrame()
+    for file in os.listdir(path):
+        print(file)
+        df = pd.concat([df, pd.read_csv(f"{path}/{file}")])
+    df.to_csv("./dataset.csv")
+
+
+
+if __name__ == "__main__":
+    setup_config()
+    
+    
+    
+    
+    
+    
+    
+    
